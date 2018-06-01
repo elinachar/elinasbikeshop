c931c1ac48573644039d84020f866c9ee7584b15
elinachar <elinachar@gmail.com>
2018-06-01 17:25:56 +0300

    4.6 Style Orders index and show page, new links and icons in Products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.includes(:product).all # Optimize query to call once the orders and the products, much faster 
   end
 
   def show
+    @order = Order.find(params[:id])
   end
-  
+
   def new
   end
 
