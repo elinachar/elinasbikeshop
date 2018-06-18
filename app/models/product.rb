50cca95928915df4ab4234bd2d79455ce14a5244
elinachar <elinachar@gmail.com>
2018-06-18 13:44:28 +0300

    5.2 Search form for products name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
     has_many :orders
+
+    def self.search(search_term)
+      if Rails.env.development?
+        where("name LIKE ?", "%#{search_term}%")
+      elsif Rails.env.production?
+        where("name ILIKE ?", "%#{search_term}%")
+      end
+    end
 end
