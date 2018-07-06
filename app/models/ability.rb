879bc464898cb233ec0f6d3b0e8572e69d4e65c7
elinachar <elinachar@gmail.com>
2018-07-06 21:20:25 +0300

    5.10 Admin Role and Delete Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :manage, User, id: user.id
+    can :read, :all # permissions for every user, even if not logged in    
+    if user.present?  # additional permissions for logged in users (they can manage their posts)
+      can :manage, User, id: user.id
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
 end
