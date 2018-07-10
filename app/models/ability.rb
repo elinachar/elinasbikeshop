3ebfe01ebb8bcd4b4aaf1c7a4f94257a2b7929bf
elinachar <elinachar@gmail.com>
2018-07-11 02:36:33 +0300

    5.11 Define acesses of users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :manage, User, id: user.id
     can :read, :all
     if user.present?
-      can :manage, Post, user_id: user.id
+      can :manage, User, id: user.id
       if user.admin?
         can :manage, :all
       end
