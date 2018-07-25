5fc42bcfe0f15c2be5f555cf899011ffb958e594
elinachar <elinachar@gmail.com>
2018-07-26 01:07:16 +0300

    6.10 Clear text area for leaving again comment,simpify style of comments-hr

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).on("turbolinks:load", function() {
   //Show form for leaving new comment
   $("#create-new-comment-btn").click(function() {
     $(".new-comment-thank-you").hide();
+    $(".new-comment-text").val("")
     $("#new_comment").slideDown("slow");
     $("#create-new-comment-btn").prop("disabled", true);
   });
